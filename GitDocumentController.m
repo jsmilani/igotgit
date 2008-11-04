81174fe858718eebd29eb623be0ce7a1ef9a1516
John Smilanick <jsmilani@Macintosh.local>
2008-11-03 23:14:07 -0800

    first commit. no useful functionality

---
+++ GitDocumentController.m
@@ -0,0 +1,42 @@
+//
+//  GitDocumentController.m
+//  iGotGit
+//
+//  Created by John Smilanick on 11/2/08.
+//  Copyright 2008 Professional Aptitude Council. All rights reserved.
+//
+
+#import "GitDocumentController.h"
+
+
+@implementation GitDocumentController
+- (NSString *)typeForContentsOfURL:(NSURL *)inAbsoluteURL error:(NSError **)outError {
+  BOOL dir;
+  if([[NSFileManager defaultManager] fileExistsAtPath:[inAbsoluteURL path] isDirectory: &dir] && dir)
+    return @"GitRepo";
+  else
+    return [super typeForContentsOfURL:inAbsoluteURL error:outError];
+}
+
+- (IBAction)openDocument:(id)sender {
+  //[[NSDocumentController sharedDocumentController] openDocument:self];
+  //return;
+  NSOpenPanel *open_panel = [NSOpenPanel openPanel];
+  [open_panel setTitle:@"Open Git Repository"];
+  [open_panel setCanChooseDirectories:YES];
+  [open_panel setCanChooseFiles:NO];
+  [open_panel setDelegate:self];
+  int result = [open_panel runModal];
+  if(result == NSOKButton) {
+    NSError *error;
+    NSDocument *doc = [self openDocumentWithContentsOfURL:[NSURL URLWithString:[[open_panel filenames] lastObject]] display:YES error:&error];
+    if(!doc)
+      return;
+  }
+}
+
+#- (IBAction)newDocument:(id)sender {
+#  NSSavePanel *save_panel = [NSSavePanel savePanel];
+#  [save_panel runModal];
+#}
+@end
---
+++ rb_main.rb
@@ -0,0 +1,33 @@
+#
+#  rb_main.rb
+#  iGotGit
+#
+#  Created by John Smilanick on 11/1/08.
+#  Copyright (c) 2008 Professional Aptitude Council. All rights reserved.
+#
+
+require 'osx/cocoa'
+include OSX
+ENV['PATH'] += ':/usr/local/git/bin'
+
+if ENV['DEBUG']
+  print 'debug'
+  require 'rubygems'
+  gem 'ruby-debug'
+end
+
+# Add our lib directory to ruby search path
+
+def rb_main_init
+  path = OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+  rbfiles = Dir.entries(path).select {|x| /\.rb\z/ =~ x}
+  rbfiles -= [ File.basename(__FILE__) ]
+  rbfiles.each do |path|
+    require( File.basename(path) )
+  end
+end
+
+if $0 == __FILE__ then
+  rb_main_init
+  OSX.NSApplicationMain(0, nil)
+end
